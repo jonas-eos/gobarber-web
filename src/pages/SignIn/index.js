2f79879c64c50288323fd7c2682f9b356607d49e
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-07 00:27:49 -0300

    chore: validations

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Enter a valid email address!')
+    .required('Email is required!'),
+  password: Yup.string().required('Password is required!'),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -12,7 +20,7 @@ export default function SignIn() {
   return (
     <section>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Your mail address" />
         <Input name="password" type="password" placeholder="Your password" />
         <button type="submit">Login</button>
