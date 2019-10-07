baa5926dc2a3299ba12395926aed89821207e77d
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-06 21:44:30 -0300

    chore: signin page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
   return (
     <section>
-      <h1>SignIn</h1>
+      <img src={logo} alt="GoBarber" />
+      <form action="submit">
+        <input type="email" placeholder="Your mail address" />
+        <input type="password" placeholder="Your password" />
+        <button type="submit">Login</button>
+        <Link to="/register">Create free account</Link>
+      </form>
     </section>
   );
 }
