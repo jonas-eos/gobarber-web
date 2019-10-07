c4e5363b26de0d06d4932e9b6594bd96425d64dd
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-07 20:54:21 -0300

    chore: signIn request intercept by sagas

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,39 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-export default all([]);
+import history from '~/services/history';
+import api from '~/services/api';
+
+import { signInSuccess } from './actions';
+
+/**
+ * Performs user authentication.
+ * For the user to be successfully authenticated and redirected to the
+ * dashboard, they must be a provider.
+ * The user data are passed through the signInSuccess, passing the user access
+ * token and its information that comes from the API.
+ * @generator @function
+ * @param {object} payload
+ * @return redirection to dashboard
+ */
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  // TODO steps if user isn't a provider
+  if (!user.provider) {
+    console.tron.error('User is not a provider!'); // eslint-disable-line
+    return 0;
+  }
+
+  yield put(signInSuccess(token, user));
+
+  return history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
