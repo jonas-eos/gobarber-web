e0f8f26530265e52d250953a321d8f5af452bb04
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-06 22:00:35 -0300

    chore: handles and unform component

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <section>
       <img src={logo} alt="GoBarber" />
-      <form action="submit">
-        <input type="email" placeholder="Your mail address" />
-        <input type="password" placeholder="Your password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Your mail address" />
+        <Input name="password" type="password" placeholder="Your password" />
         <button type="submit">Login</button>
         <Link to="/register">Create free account</Link>
-      </form>
+      </Form>
     </section>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <section>
       <img src={logo} alt="GoBarber" />
-      <form action="submit">
-        <input placeholder="Full name" />
-        <input type="email" placeholder="Your mail address" />
-        <input type="password" placeholder="Password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your mail address" />
+        <Input name="password" type="password" placeholder="Password" />
         <button type="submit">Create account</button>
         <Link to="/">Already registered</Link>
-      </form>
+      </Form>
     </section>
   );
 }
