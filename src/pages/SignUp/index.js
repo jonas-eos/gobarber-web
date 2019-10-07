794a6ab30bb07c627cefbf997d18b55b7bbf6da9
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-06 21:51:44 -0300

    chore: signup page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
   return (
     <section>
-      <h1>SignUp</h1>
+      <img src={logo} alt="GoBarber" />
+      <form action="submit">
+        <input placeholder="Full name" />
+        <input type="email" placeholder="Your mail address" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Create account</button>
+        <Link to="/">Already registered</Link>
+      </form>
     </section>
   );
 }
