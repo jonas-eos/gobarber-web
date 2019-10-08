37915c81ca80f62d8e8a9dc339ba5c08a0272a87
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-07 21:22:09 -0300

    chore: handle reducer states from sign in success

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,9 +1,24 @@
+import produce from 'immer';
+
+/** Global state */
 const INITIAL_STATE = {
-  token: '123',
+  token: null,
+  signed: false,
+  loading: false,
 };
 
 export default function auth(state = INITIAL_STATE, action) {
   switch (action.type) {
+    /** Start user section with a token */
+    case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, draft => {
+        const newState = draft;
+
+        newState.token = action.payload.token;
+        newState.signed = true;
+      });
+
+    /** Return global state if any other action to auth are requested */
     default:
       return state;
   }
