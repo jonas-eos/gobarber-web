5cadfe43670ec2d423341000431bfe4826e99d10
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-09 20:05:20 -0300

    chore: profile page markup + styles

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
   return (
-    <section>
-      <h1>Profile</h1>
-    </section>
+    <Container>
+      <Form>
+        <Input name="name" placeholder="Full Name" />
+        <Input name="email" type="email" placeholder="Your email address" />
+        <hr />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Current password"
+        />
+        <Input type="password" name="password" placeholder="New password" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm password"
+        />
+        <button type="submit">Update profile</button>
+      </Form>
+      <button type="button">Logout</button>
+    </Container>
   );
 }
