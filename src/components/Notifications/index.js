a4850cddb5cdbd5a2bc7b203b75dcc488d2bf42c
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-08 22:11:51 -0300

    chore: notification information and stylization

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 
 import { MdNotifications } from 'react-icons/md';
-import { Container, Badge } from './styles';
+import { Container, Badge, NotificationList, Notification } from './styles';
 
 export default function Notifications() {
   return (
     <Container>
-      <Badge>
+      <Badge hasUnread>
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
+
+      <NotificationList>
+        <Notification unread>
+          <p>You have new schedule for tomorow</p>
+          <time>2 days later</time>
+          <button type="button"> mark as readed</button>
+        </Notification>
+        <Notification>
+          <p>You have new schedule for tomorow</p>
+          <time>2 days later</time>
+          <button type="button"> mark as readed</button>
+        </Notification>
+        <Notification>
+          <p>You have new schedule for tomorow</p>
+          <time>2 days later</time>
+          <button type="button"> mark as readed</button>
+        </Notification>
+      </NotificationList>
     </Container>
   );
 }
