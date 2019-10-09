ee287bc0e7d7a9de45dfabc5e1e674c5f2dd49e7
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-09 20:11:31 -0300

    chore: load profile state in form

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
 import { Container } from './styles';
 
+/**
+ * Page to change profile data.
+ * The user can change all their informations.
+ * To change the password, the user must enter the new password and confirm the
+ * new password for the change to take place.
+ * @function
+ * @default
+ */
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
   return (
     <Container>
-      <Form>
+      <Form initialData={profile} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Full Name" />
         <Input name="email" type="email" placeholder="Your email address" />
         <hr />
