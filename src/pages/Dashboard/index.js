affe70f6d7f4ed1c2b8d546e11402749390f12a4
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-12 17:35:04 -0300

    chore: browser between days

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, addDays, subDays } from 'date-fns';
+import { enUS } from 'date-fns/locale/';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  /** The date format should be shown equal to the marked locale pattern. */
+  const dateFormatted = useMemo(
+    () => format(date, `do MMMM`, { locale: enUS }),
+    [date]
+  );
+
+  /**
+   * Call prev day on button Click
+   * @function
+   */
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  /**
+   * Call next day on button Click
+   * @function
+   */
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handlePrevDay}>
           <MdChevronLeft size={36} color="#fff" />
         </button>
-        <strong>31th march</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
           <MdChevronRight size={36} color="#fff" />
         </button>
       </header>
-
       <section>
         <ul>
           <Time past>
